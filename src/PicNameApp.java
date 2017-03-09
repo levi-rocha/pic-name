9489c15ccf34e290df6590340fd3e9bf914a0a3a
Levi Rocha <ilevidr@gmail.com>
2017-03-09 18:32:19 -0300

    updated main app

--- src/PicNameApp.java
+++ src/PicNameApp.java
@@ -29,7 +29,7 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 
-public class BigDumbTestMain {
+public class PicNameApp {
 
   static List<File> imgs;
   static JFrame editorFrame;
